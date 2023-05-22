54910fdc2f4089a741d1c0edee06b4e125e7ad4e
alikutluca <alikutluca@yahoo.com>
2023-05-22 17:18:56 +0300

    fix: first sceleton created

---
+++ components/Nav.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import {} from "next-auth/react";
+
+const Nav = () => {
+  return <div>asd</div>;
+};
+
+export default Nav;
